3ab73550e539454cb43ebb34ffb4b70168483639
Angelo Pirola <angelo9871@gmail.com>
2023-12-08 00:02:11 +0100

    Aggiunto progetto principale WebApp

---
+++ src/WebApp/Shared/UserDisplay.razor.cs
@@ -0,0 +1,24 @@
+﻿namespace GestioneSagre.Web.App.Shared;
+
+public partial class UserDisplay
+{
+    private readonly int numberNotifications = FrontendParameters.NOTIFICATION_NUMBER;
+
+    protected override async Task OnInitializedAsync() => await base.OnInitializedAsync();
+    protected override async Task OnParametersSetAsync()
+    {
+        await base.OnParametersSetAsync();
+
+        //OnChangeNumber(0);
+    }
+
+    private void OpenAboutPage() => Navigation.NavigateTo("/about");
+
+    //private void StartSupportChat() => Navigation.NavigateTo(FrontendParameters.TELEGRAM_SUPPORT, true);
+
+    // private void OnChangeNumber(int value)
+    // {
+    //     NumberNotifications = value;
+    //     StateHasChanged();
+    // }
+}
