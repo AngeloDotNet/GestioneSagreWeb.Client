5ec32734b5444214db012b96972219f29575f8a5
Angelo Pirola <angelo9871@gmail.com>
2023-12-07 10:16:07 +0100

    Aggiunto modulo Configurazione Iniziale

---
+++ src/ModuliWeb/GestioneSagre.Modules.ConfigurazioneIniziale/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using GestioneSagre.Web.Models.ConfigurazioneIniziale;
+global using GestioneSagre.Web.Models.ConfigurazioneIniziale.InputModels;
+global using GestioneSagre.Web.Services;
+global using GestioneSagre.Web.Services.ConfigurazioneIniziale;
+global using Microsoft.AspNetCore.Components;
+global using MudBlazor;
---
+++ src/ModuliWeb/GestioneSagre.Modules.ConfigurazioneIniziale/Pages/ConfigurazioneIniziale.razor.cs
@@ -0,0 +1,203 @@
+﻿namespace GestioneSagre.Modules.ConfigurazioneIniziale.Pages;
+
+public partial class ConfigurazioneIniziale
+{
+    [Inject] public IConfigurazioneInizialeService Service { get; set; }
+    [Inject] public NavigationManager Navigation { get; set; }
+    [Inject] public ISnackbar Snackbar { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        await LoadDatiAsync();
+    }
+
+    private readonly List<BreadcrumbItem> items = new List<BreadcrumbItem>
+    {
+        new BreadcrumbItem("Home Page", href: "/"),
+        new BreadcrumbItem("Configurazione Iniziale", href: null, disabled: true)
+    };
+
+    private List<ConfigInizialeViewModel> listItems = new();
+    private string errorMessage;
+
+    private bool isLoading = false;
+    private bool disableBtnFesta = false;
+
+    private readonly bool disableBtnAttivaFesta = FrontendParameters.DISABLE_BUTTONS; //true;
+    private readonly bool disableBtnDisattivaFesta = FrontendParameters.DISABLE_BUTTONS; //true;
+    private readonly bool disableBtnModificaFesta = false;
+    private readonly bool disableBtnTerminaFesta = FrontendParameters.DISABLE_BUTTONS; //true;
+    private readonly bool disableBtnEliminaFesta = FrontendParameters.DISABLE_BUTTONS; //true;
+
+    private async Task LoadDatiAsync()
+    {
+        try
+        {
+            isLoading = true;
+            listItems = await Service.GetListaFeste();
+
+            var EnabledMenu = await Service.VerifyStatusMenu();
+            await Service.ManagementNavMenuAsync(EnabledMenu);
+            StateHasChanged();
+
+            if (listItems.Any(x => x.StatusFesta is FestaStatus.Active or FestaStatus.Inactive))
+            {
+                disableBtnFesta = true;
+                StateHasChanged();
+            }
+            else
+            {
+                disableBtnFesta = false;
+                StateHasChanged();
+            }
+        }
+        catch (ApplicationException ex)
+        {
+            errorMessage = ex.Message;
+        }
+        finally
+        {
+            isLoading = false;
+        }
+    }
+
+    private async Task AttivaFestaAsync(ConfigInizialeViewModel model)
+    {
+        try
+        {
+            FestaInputModel festa = new()
+            {
+                Id = model.Id,
+                DataInizio = model.DataInizio.Value,
+                DataFine = model.DataFine.Value,
+                StatusFesta = FestaStatus.Active
+            };
+
+            isLoading = true;
+            if (!await Service.UpdateFestaAsync(festa))
+            {
+                Snackbar.Add("Errore durante l'attivazione della festa !", Severity.Error);
+            }
+
+            Snackbar.Add("Festa attivata con successo !", Severity.Success);
+
+            await ExecuteLastStepsAsync();
+        }
+        catch (ApplicationException ex)
+        {
+            errorMessage = ex.Message;
+        }
+        finally
+        {
+            isLoading = false;
+        }
+    }
+
+    private async Task DisattivaFestaAsync(ConfigInizialeViewModel model)
+    {
+        try
+        {
+            FestaInputModel festa = new()
+            {
+                Id = model.Id,
+                DataInizio = model.DataInizio.Value,
+                DataFine = model.DataFine.Value,
+                StatusFesta = FestaStatus.Inactive
+            };
+
+            isLoading = true;
+            if (!await Service.UpdateFestaAsync(festa))
+            {
+                Snackbar.Add("Errore durante la disattivazione della festa !", Severity.Error);
+            }
+
+            Snackbar.Add("Festa disattivata con successo !", Severity.Success);
+
+            await ExecuteLastStepsAsync();
+        }
+        catch (ApplicationException ex)
+        {
+            errorMessage = ex.Message;
+        }
+        finally
+        {
+            isLoading = false;
+        }
+    }
+
+    private void ModificaFestaAsync(ConfigInizialeViewModel model)
+        => Navigation.NavigateTo(errorMessage is null ? $"/festa/{model.Id}" : "/configurazioneiniziale");
+
+    private async Task TerminaFestaAsync(ConfigInizialeViewModel model)
+    {
+        try
+        {
+            FestaInputModel festa = new()
+            {
+                Id = model.Id,
+                DataInizio = model.DataInizio.Value,
+                DataFine = model.DataFine.Value,
+                StatusFesta = FestaStatus.Finished
+            };
+
+            isLoading = true;
+            if (!await Service.UpdateFestaAsync(festa))
+            {
+                Snackbar.Add("Errore durante la chiusura della festa !", Severity.Error);
+            }
+
+            Snackbar.Add("Festa terminata con successo !", Severity.Success);
+
+            await ExecuteLastStepsAsync();
+        }
+        catch (ApplicationException ex)
+        {
+            errorMessage = ex.Message;
+        }
+        finally
+        {
+            isLoading = false;
+        }
+    }
+
+    private async Task EliminaFestaAsync(ConfigInizialeViewModel model)
+    {
+        try
+        {
+            FestaInputModel festa = new()
+            {
+                Id = model.Id,
+                DataInizio = model.DataInizio.Value,
+                DataFine = model.DataFine.Value,
+                StatusFesta = FestaStatus.Deleted
+            };
+
+            isLoading = true;
+            if (!await Service.UpdateFestaAsync(festa))
+            {
+                Snackbar.Add("Errore durante l'eliminazione della festa !", Severity.Error);
+            }
+
+            Snackbar.Add("Festa eliminata con successo !", Severity.Success);
+
+            await ExecuteLastStepsAsync();
+        }
+        catch (ApplicationException ex)
+        {
+            errorMessage = ex.Message;
+        }
+        finally
+        {
+            isLoading = false;
+        }
+    }
+
+    private async Task ExecuteLastStepsAsync()
+    {
+        await LoadDatiAsync();
+
+        await Task.Delay(1500);
+        Navigation.NavigateTo("/", true);
+    }
+}
