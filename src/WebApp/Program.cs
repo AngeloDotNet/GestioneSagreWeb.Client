bf9e1f612f7fb8e478fe9972f40d0f7bffba92cb
Angelo Pirola <angelo9871@gmail.com>
2023-12-09 12:36:27 +0100

    Risoluzione bug #4 closes #4

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -20,7 +20,7 @@ public class Program
                 config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
             });
 
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://api-gateway:5001") });
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://sagre-api.aepserver.it") });
 
         builder.Services.Scan(scan => scan.FromAssemblyOf<IConfigurazioneInizialeService>()
             .AddClasses(services => services.Where(type => type.Name.EndsWith("Service")))
