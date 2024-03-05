914f8004ff240775a1f6ea5b7417e9079797cf45
Angelo Pirola <angelo9871@gmail.com>
2024-03-05 20:45:06 +0100

    Aggiunto ViewModels Categorie

---
+++ src/WebApp.Models/Categorie/ViewModels/CategoriaMinimalViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace GestioneSagre.Web.Models.Categorie.ViewModels;
+
+public class CategoriaMinimalViewModel
+{
+    public Guid Id { get; set; }
+    public string CategoriaVideo { get; set; }
+}
