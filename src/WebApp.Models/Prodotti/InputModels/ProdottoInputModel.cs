970449bc82b17a6d50582af41af4a4d279cc757d
Angelo Pirola <angelo9871@gmail.com>
2024-03-05 20:45:46 +0100

    Aggiunto InputModels Prodotti

---
+++ src/WebApp.Models/Prodotti/InputModels/ProdottoInputModel.cs
@@ -0,0 +1,16 @@
+﻿namespace GestioneSagre.Web.Models.Prodotti.InputModels;
+
+public class ProdottoInputModel
+{
+    public Guid Id { get; set; }
+    public Guid IdFesta { get; set; }
+    public string Descrizione { get; set; }
+    public bool ProdottoAttivo { get; set; }
+    public Guid IdCategoria { get; set; }
+    public virtual Price Prezzo { get; set; }
+    public int Quantita { get; set; }
+    public bool QuantitaAttiva { get; set; }
+    public int QuantitaScorta { get; set; }
+    public bool AvvisoScorta { get; set; }
+    public bool Prenotazione { get; set; }
+}
