7cfb7acae04f7701aab5e9809267b294c8b91093
Angelo Pirola <angelo9871@gmail.com>
2024-03-09 12:55:27 +0100

    Refactoring

---
+++ src/ModuliWeb/GestioneSagre.Modules.Prodotti/GlobalUsings.cs
@@ -0,0 +1,11 @@
+﻿global using Blazored.LocalStorage;
+global using GestioneSagre.Web.Models.Categorie.ViewModels;
+global using GestioneSagre.Web.Models.Prodotti;
+global using GestioneSagre.Web.Models.Prodotti.InputModels;
+global using GestioneSagre.Web.Models.Prodotti.ViewModels;
+global using GestioneSagre.Web.Models.ValueObjects;
+global using GestioneSagre.Web.Services;
+global using GestioneSagre.Web.Services.Generico;
+global using GestioneSagre.Web.Services.Prodotti;
+global using Microsoft.AspNetCore.Components;
+global using MudBlazor;
